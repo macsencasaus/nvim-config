3372d7653d1699cb289ebd791ff23468fc7b5b35
Macsen Casaus <macsencasaus@gmail.com>
2026-03-11 21:18:38 -0500

    image + jupy

---
+++ lua/macsen/plugins/jupytext.lua
@@ -0,0 +1,11 @@
+return {
+	"GCBallesteros/jupytext.nvim",
+	config = {
+		style = "hydrogen",
+		output_extension = "auto", -- Default extension. Don't change unless you know what you are doing
+		force_ft = nil, -- Default filetype. Don't change unless you know what you are doing
+		custom_language_formatting = {},
+	},
+	-- Depending on your nvim distro or config you may need to make the loading not lazy
+	-- lazy=false,
+}
